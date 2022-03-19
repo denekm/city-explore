d514069186e105c11f3b95f3742206e23c3db4aa
Marta <mrtdeneke@gmail.com>
2022-03-18 18:29:07 -0700

    worked on map display

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     super(props);
     this.state ={
       searchQuery:'',
-      locationObj: {}
+      locationObj: {},
+      map:''
     }
 
   }
@@ -16,6 +17,7 @@ class App extends React.Component {
     const response = await axios.get(url);
     console.log(response);
     this.setState({locationObj: response.data[0]});
+    this.setState({map:`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12`})
   }
   render() {
     return (
@@ -28,7 +30,10 @@ class App extends React.Component {
    <h3> The city you searched for was: {this.state.locationObj.display_name}</h3>
    <h3> latitude: {this.state.locationObj.lat}</h3>
    <h3> longitude: {this.state.locationObj.lat}</h3>
+   <img src={this.state.map} alt={this.state.locationObj.display_name} title={this.state.locationObj.display_name}/>
    </>
+
+
    }
    </div >
     );
