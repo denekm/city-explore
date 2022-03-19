9fecbce95c23c55e03c4546e01a5a136ffae7d93
Marta <mrtdeneke@gmail.com>
2022-03-18 17:30:21 -0700

    worked on displaying the lat and lon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state ={
-      searchQuery:''
+      searchQuery:'',
+      locationObj: {}
     }
 
   }
-  getLocatipn = async() => {
-    const url = 'https://us1.locationiq.com/v1/search.php?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json'
+  getLocation = async() => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
+    const response = await axios.get(url);
+    console.log(response);
+    this.setState({locationObj: response.data[0]});
   }
   render() {
     return (
       <div className='App'>
-        <input onChange={(event) => this.setState({searchQuery: event.target.value})} placeholder= 'look up a city'></input>
-   < button onClick ={this.getLocation}> Explore</button>
+        <input onChange={(event) => this.setState({searchQuery: event.target.value})} placeholder= 'type a city'></input>
+   < button onClick ={this.getLocation}> Explore!</button>
+
+   {this.state.locationObj.display_name && 
+   <>
+   <h3> The city you searched for was: {this.state.locationObj.display_name}</h3>
+   <h3> latitude: {this.state.locationObj.lat}</h3>
+   <h3> longitude: {this.state.locationObj.lat}</h3>
+   </>
+   }
    </div >
     );
   }
