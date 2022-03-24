4c570ef970e38b7c41025b6580bf4bffb36c2fb2
Marta <mrtdeneke@gmail.com>
2022-03-24 14:27:19 -0700

    lab 07

---
+++ src/Weather.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+class Weather extends React.Component {
+
+  render() {
+    console.log(this.props.forecastData);
+    return (
+      <>
+        <h4> Weather Data: </h4>
+        {this.props.forecastData.map(day => (
+          <div key={this.props.forecastData.indexOf(day)}>
+          <p>{day.date}</p>
+          <p> {day.description}</p>
+          </div>
+        ))}
+      </>
+    )
+  }
+}
+
+export default Weather;
